baa618e539bad5a20890b522c8d70237a2f3ac1f
daniel117622 <dan5jul@hotmail.com>
2022-12-03 23:57:56 -0600

    Progress bar

--- app/src/main/java/com/example/mybank/MainActivity.java
+++ app/src/main/java/com/example/mybank/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.mybank;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -12,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     MyViewPagerAdapter myViewPagerAdapter;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +26,27 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tab_layout);
         viewPager2 = findViewById(R.id.view_pager);
         myViewPagerAdapter = new MyViewPagerAdapter(this);
-
+        progressBar = findViewById(R.id.progress);
         viewPager2.setAdapter(myViewPagerAdapter);
+
+        progressBar.setProgress(0);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager2.setCurrentItem(tab.getPosition());
+                new Thread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        Looper.prepare();
+                        /* ObjectAnimator.ofFloat(button,"translationX",0,300).setDuration(300).start(); */
+                        int init = progressBar.getProgress();
+                        ObjectAnimator.ofInt(progressBar,"progress",init + 10).setDuration(300).start();
+                        Log.d("Animation",String.valueOf(init));
+                        Looper.loop();
+                    }
+                }).start();
             }
 
             @Override
