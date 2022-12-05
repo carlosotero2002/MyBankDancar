850596bb72fbf527ea154d0e11575d522f58a9a6
daniel117622 <dan5jul@hotmail.com>
2022-12-04 18:09:22 -0600

    Transaccion table view

--- app/src/main/java/com/example/mybank/fragments/TransferFragment.java
+++ app/src/main/java/com/example/mybank/fragments/TransferFragment.java
@@ -1,16 +1,23 @@
 package com.example.mybank.fragments;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.mybank.LoginActivity;
+import com.example.mybank.MainActivity;
 import com.example.mybank.R;
 
 public class TransferFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,4 +25,34 @@ public class TransferFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_transfer, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        SQLiteDatabase db = LoginActivity.db;
+        Cursor c = db.rawQuery("SELECT * FROM Transacciones Where email_origen=? OR email_destino=?", new String[] {LoginActivity.loggedUser,LoginActivity.loggedUser});
+
+        String table = "";
+        if (c != null)
+        {
+            while(c.moveToFirst())
+            {
+                String origen = c.getString(0);
+                String destino = c.getString(1);
+                String banco = c.getString(2);
+                String cantidad = c.getString(3);
+                String motivo = c.getString(4);
+
+                table += "|" + origen + "|" + destino + "|" + banco + "|" + cantidad + "|" + motivo + "\n";
+            }
+            TextView t = view.findViewById(R.id.transaccion_table);
+            t.setText(table);
+
+        }
+
+
+    }
+
 }
